4208e9bfce0defa48f19ce367623064038d83abf
Ngoc Thach <thach.le@topebox.com>
2023-08-09 14:39:30 +0700

    quiz 1 part2

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
+	"time"
+)
+
+const (
+	TIME_LIMIT = 30
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	timeLimit := flag.Int("limit", TIME_LIMIT, "the time limit for the quiz in seconds")
+	flag.Parse()
 
 	// read problems.csv file
 	file, err := os.Open("problems.csv")
@@ -30,28 +37,45 @@ func main() {
 	score := 0
 
 	fmt.Println("--- Quiz ---")
+	fmt.Print("Press enter to start")
+	fmt.Scanln()
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 	for _, record := range records {
+
+		// print question
 		fmt.Print(record[0] + " = ")
-		var answer string
-		fmt.Scanln(&answer)
+		ansChan := make(chan int)
 
-		// parse answer to int
-		answerInt, err := strconv.Atoi(answer)
-		if err != nil {
-			fmt.Println("Error parsing answer")
-			os.Exit(1)
-		}
+		go func() {
+			var answer string
+			fmt.Scanln(&answer)
+			// parse answer to int
+			answerInt, err := strconv.Atoi(answer)
+			if err != nil {
+				fmt.Println("Error parsing answer")
+				os.Exit(1)
+			}
+			ansChan <- answerInt
+		}()
 
-		// parse record[1] to int
-		recordInt, err := strconv.Atoi(record[1])
-		if err != nil {
-			fmt.Println("Error parsing result")
-			os.Exit(1)
-		}
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's up!")
+			fmt.Println("Your score is", score)
+			return
+		case answerInt := <-ansChan:
+			// parse record[1] to int
+			recordInt, err := strconv.Atoi(record[1])
+			if err != nil {
+				fmt.Println("Error parsing result")
+				os.Exit(1)
+			}
 
-		if answerInt == recordInt {
-			score++
+			if answerInt == recordInt {
+				score++
+			}
 		}
 	}
 
